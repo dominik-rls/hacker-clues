fa08d4f6b8be8084ad9953981cdb5aa33a24618b
drosehnal <drosehnal@relias.com>
2023-03-29 12:30:33 +0200

    Project set up, ready to go!

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-slate-800">
+      <h1 className="text-3xl font-bold text-slate-100">
+      Hello, World!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
